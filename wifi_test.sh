0207cce90f5cebe924a3d82c8f124d4a757f123e
SpaceMonkey <99768453+LeadHand40K@users.noreply.github.com>
2025-12-07 18:26:56 +0200

    Add script to enable monitor mode and test injection

--- wifi_test.sh
+++ wifi_test.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
+
+
 echo "=== Enabling Monitor Mode ==="
+
+
+sudo ifconfig wlan0 down
+sudo airmon-ng check kill
+sudo iwconfig wlan0 mode monitor
+sudo ifconfig wlan0 up
+
+
+echo "=== Wireless Status ==="
+iwconfig
+
+
+echo "=== Testing Packet Injection ==="
+sudo aireplay-ng --test wlan0
